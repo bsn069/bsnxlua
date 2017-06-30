5d1aa731c8207981e63757fff3a28ff1912e2db5
putao <513026809@qq.com>
2017-06-30 11:18:58 +0800

    事实上

--- setup.bat
+++ setup.bat
@@ -2,10 +2,39 @@
 
 if not exist nogit (
 	mkdir nogit
+	if not exist nogit (
+		echo not found dir nogit in path %CD%
+		exit 1
+	) 
 ) 
 
-pushd nogit
-if not exist xlua (
+if not exist nogit/xLua (
 	git clone https://github.com/Tencent/xLua.git
+	if not exist nogit/xlua (
+		echo not found dir nogit/xlua in path %CD%
+		exit 1
+	) 
+
+	if exist nogit/xLuaTmp (
+		rm -rf nogit/xLuaTmp 
+	)
+	cp -r nogit/xLua/Assets/XLua nogit/XLuaTmp
+	cp -r nogit/xLua/Assets/Plugins nogit/XLuaTmp/Plugins
+	rm -rf nogit/xLuaTmp/Doc	
+	rm -rf nogit/xLuaTmp/Examples	
+	rm -rf nogit/xLuaTmp/Tutorial	
+	del /s /q nogit\xLuaTmp\*.meta
+
+	if not exist u3d/Assets/XLua (
+		mkdir u3d\Assets\XLua
+		if not exist u3d/Assets/XLua (
+			echo not found dir u3d/Assets/XLua in path %CD%
+			exit 1
+		) 
+	) 
+	cp -r nogit/xLuaTmp/* u3d/Assets/XLua
+	rm -rf nogit/xLuaTmp
 ) 
-popd
+
+
+pause
