a4c92aa4394c435058e7584ff5850a7668cc64e7
putao <513026809@qq.com>
2017-07-03 14:18:15 +0800

    bsn

---
+++ u3d/Assets/bsn/Global.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+// using LuaInterface;
+using System.Runtime.InteropServices;
+using System;
+
+namespace NBsn {
+    // [Reg2LuaAttribute]
+    public class CGlobal : IDisposable {
+        public static NBsn.CGlobal Instance {
+            get { 
+                #if UNITY_EDITOR
+                if (m_instance == null) {
+                    new CGlobal();
+                }
+                #endif
+                return m_instance; 
+            }
+        }
+
+        public NBsn.CLog Log {
+            get { return m_Log; }
+        }
+
+		public NBsn.MMain Main {
+            get { return m_Main; }
+        }
+
+        public UnityEngine.GameObject GoMain {
+            get { return m_goMain; }
+        }
+
+        public UnityEngine.Transform TfMain {
+            get { return m_tfMain; }
+        }
+
+        public void ConfigInit( ) {
+            Log.Info("NBsn.CGlobal.ConfigInit()"); 
+        }
+
+        #region game init
+        // 游戏逻辑初始化
+        public void AppInit(GameObject goMain, NBsn.MMain Main) {
+            Log.Info("NBsn.CGlobal.AppInit()"); 
+
+            m_goMain    = goMain;
+            m_Main      = Main;
+            m_tfMain    = m_goMain.transform;
+        }
+
+        public void AppUnInit() {
+            Log.Info("NBsn.CGlobal.AppUnInit()"); 
+
+            m_tfMain    = null;
+            m_Main      = null;
+            m_goMain    = null;
+        }
+        #endregion
+
+        public void StartApp() {
+            Log.Info("NBsn.CGlobal.StartApp()"); 
+        }
+
+        #region
+        public CGlobal() {
+            m_instance = this;
+        }
+        public void Dispose() {
+            Log.Info("NBsn.CGlobal.Dispose()"); 
+            m_instance = null;
+        }
+
+        protected static CGlobal m_instance = null;
+
+        protected NBsn.CLog m_Log = new NBsn.CLog();
+
+        protected NBsn.MMain m_Main = null;
+        protected GameObject m_goMain = null;
+        protected Transform m_tfMain = null;
+        #endregion
+    }
+}
---
+++ u3d/Assets/bsn/Log.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+// using LuaInterface;
+// using System.Runtime.InteropServices;
+using System;
+
+namespace NBsn {
+    // [Reg2LuaAttribute]
+    public class CLog {
+        public void InfoFormat(string format, params object[] args) {
+            Debug.LogFormat(format, args);
+        }
+
+        public void ErrorFormat(string format, params object[] args) {
+            Debug.LogErrorFormat(format, args);
+        }
+
+        public void Info(object message) {
+            Debug.Log(message);
+        }
+
+        public void Error(object message) {
+            Debug.LogError(message);
+        }
+    }
+}
